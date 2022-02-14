e8b6ddfe2fd7ba9318c21287aaf432b7df7823cc
Yiyi <loveloveyy@hotmail.com>
2022-02-14 17:41:21 +0100

    with authorization token for uploading a offer

--- src/pages/Publish.js
+++ src/pages/Publish.js
@@ -1,51 +1,54 @@
 import "./Publish.css";
 import { useState } from "react";
 import axios from "axios";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Publish = () => {
   const [picture, setPicture] = useState();
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [brand, setBrand] = useState("");
-  const [size, setSize] = useState("");
-  const [color, setColor] = useState("");
-  const [condition, setCondition] = useState("");
-  const [location, setLocation] = useState("");
-  const [price, setPrice] = useState("");
+  const [title, setTitle] = useState("test");
+  const [description, setDescription] = useState("test");
+  const [brand, setBrand] = useState("test");
+  const [size, setSize] = useState("test");
+  const [color, setColor] = useState("test");
+  const [condition, setCondition] = useState("test");
+  const [location, setLocation] = useState("test");
+  const [price, setPrice] = useState(30);
   const [checkbox, setCheckbox] = useState(false);
 
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
   //send form info to server
 
   const handleSubmit = async (event) => {
     try {
-      event.preventdefault();
-      setIsLoading(true);
+      event.preventDefault();
+      //   setIsLoading(true);
 
       //create FormData, type of objet
-      const data = new FormData();
+      const formData = new FormData();
       //add keys/value to the objet data
-      data.append("picture", picture);
-      data.append("title", title);
-      data.append("description", description);
-      data.append("brand", brand);
-      data.append("size", size);
-      data.append("color", color);
-      data.append("condition", condition);
-      data.append("location", location);
-      data.append("price", price);
-      data.append("checkbox", checkbox);
+      formData.append("picture", picture);
+      formData.append("title", title);
+      formData.append("description", description);
+      formData.append("brand", brand);
+      formData.append("size", size);
+      formData.append("color", color);
+      formData.append("condition", condition);
+      formData.append("location", location);
+      formData.append("price", price);
+      formData.append("checkbox", checkbox);
+      console.log(formData);
 
       const response = await axios.post(
         "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
-        data
+        formData,
+        { headers: { authorization: `Bearer ${Cookies.get("userToken")}` } }
       );
-      console.log(response.data);
-      //   setData(response.data);
-      //   setIsLoading(true);
+      //   console.log(response.data);
+
+      response.data && navigate(`/offer${response.data._id}`);
     } catch (error) {
-      console.log(error.message);
+      console.log(error);
     }
   };
 
@@ -59,6 +62,7 @@ const Publish = () => {
             type="file"
             placeholder="Ajoute une photo"
             name="Ajoute"
+            // value={picture} type file n'a pas de champs value
             onChange={(event) => {
               setPicture(event.target.files[0]);
             }}
@@ -68,6 +72,7 @@ const Publish = () => {
           <input
             type="text"
             name="Titre "
+            value={title}
             placeholder="ex: Chemise Sézane verte"
             onChange={(event) => {
               setTitle(event.target.value);
@@ -76,6 +81,7 @@ const Publish = () => {
           <input
             type="text"
             name="Décris ton article"
+            value={description}
             placeholder="ex: porté quelques fois, taille correctement"
             onChange={(event) => {
               setDescription(event.target.value);
@@ -86,6 +92,7 @@ const Publish = () => {
           <input
             type="text"
             name="Marque"
+            value={brand}
             placeholder="ex: Zara"
             onChange={(event) => {
               setBrand(event.target.value);
@@ -94,6 +101,7 @@ const Publish = () => {
           <input
             type="text"
             name="Taille"
+            value={size}
             placeholder="ex: L/40/12"
             onChange={(event) => {
               setSize(event.target.value);
@@ -102,6 +110,7 @@ const Publish = () => {
           <input
             type="text"
             name="Couleur"
+            value={color}
             placeholder="ex: Fushia"
             onChange={(event) => {
               setColor(event.target.value);
@@ -110,6 +119,7 @@ const Publish = () => {
           <input
             type="text"
             name="Etat"
+            value={condition}
             placeholder="ex: Neuf avec étiquette"
             onChange={(event) => {
               setCondition(event.target.value);
@@ -118,6 +128,7 @@ const Publish = () => {
           <input
             type="text"
             name="Lieu"
+            value={location}
             placeholder="ex: Paris"
             onChange={(event) => {
               setLocation(event.target.value);
@@ -128,6 +139,7 @@ const Publish = () => {
           <input
             type="text"
             name="Prix"
+            value={price}
             placeholder="ex: 0.00 €"
             onChange={(event) => {
               setPrice(event.target.value);
